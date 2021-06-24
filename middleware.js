19994042057edb13f1fd5d7277b08ad27edeb88f
Abdulsalam Amenov <amenov.abdulsalam@gmail.com>
2021-06-24 17:09:00 +0600

    first commit

---
+++ middleware.js
@@ -0,0 +1,19 @@
+module.exports = (routes, options) => {
+  return (req, res, next) => {
+    if (req.method === 'GET') {
+      if (req.baseUrl === req.baseUrl + '/routes') {
+        return res.json({ routes, options })
+      }
+
+      if (req.originalUrl.includes(`${req.baseUrl}/assets`)) {
+        return res.sendFile(
+          __dirname + req.originalUrl.replace(req.baseUrl, '')
+        )
+      }
+
+      return res.sendFile(__dirname + '/index.html')
+    }
+
+    next()
+  }
+}
